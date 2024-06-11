361dc9f2d2981aaf0197e0c8a5ca009f11ecdcb8
MaloYi <89474742+MaloYi@users.noreply.github.com>
2024-06-11 10:14:42 -0700

    Update cypher.py
    added comments about each function output. changed name from"key" to "shift" in description function to make it more uniform

--- cypher.py
+++ cypher.py
@@ -11,17 +11,19 @@ def encrypt(text, shift):
     return encrypted_text
 
 print(encrypt("Cuantos carros choco pancho pantera?", 21))
+#output will be "xpviojn xvmmjn xcjxj kvixcj kviozmv?"
 
 #DECRYPTION
 
-def decrypt(message, key):
+def decrypt(message, shift):
     message = message.lower()
     decrypted_message = ""
     for char in message:
         if char.islower():
-            decrypted_message += chr((ord(char) - key - 97) % 26 + 97)
+            decrypted_message += chr((ord(char) - shift - 97) % 26 + 97)
         else:
             decrypted_message += char
     return decrypted_message
 
 print(decrypt("xpviojn xvmmjn xcjxj kvixcj kviozmv?", 21))
+#output will be "Cuantos carros choco pancho pantera?"
